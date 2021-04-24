a7a6d301a135f4ab529ef1e7565028efc6b0eb37
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-04-24 23:55:03 +0100

    flink input

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" />
+      <button>Shorten</button>
+      <input type="text" />
+      <button>copy Link</button>
+    </div>
+  );
+}
+
+export default App;
