b111493940597392dc31df3365fa2192d8f58498
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-04-25 23:50:12 +0100

    fbityl shortner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const token = process.env.Access_TOKEN;
+const url = "https://api-ssl.bitly.com/v4/shorten";
 function App() {
+  const generateLink = (e) => {
+    e.preventDefault();
+    const longLink = e.target.newLink.value;
+    console.log(longLink);
+    fetch("https://api-ssl.bitly.com/v4/shorten", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ long_url: longLink, domain: "bit.ly" }),
+    }).then((res) => console.log(res));
+  };
+
   return (
     <div className="App">
-      <input type="text" />
-      <button>Shorten</button>
+      <form action="" onSubmit={generateLink}>
+        <input type="text" name="newLink" />
+        <button>Shorten</button>
+      </form>
       <input type="text" />
       <button>copy Link</button>
     </div>
