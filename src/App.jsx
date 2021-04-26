a406135335749a7538270c0d6308485fadebf0fd
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-04-26 23:55:08 +0100

    fform style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const token = process.env.Access_TOKEN;
+const token = process.env.REACT_APP_Access_TOKEN;
+
 const url = "https://api-ssl.bitly.com/v4/shorten";
 function App() {
   const generateLink = (e) => {
     e.preventDefault();
     const longLink = e.target.newLink.value;
     console.log(longLink);
+    console.log(token);
     fetch("https://api-ssl.bitly.com/v4/shorten", {
       method: "POST",
       headers: {
@@ -15,7 +17,9 @@ function App() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ long_url: longLink, domain: "bit.ly" }),
-    }).then((res) => console.log(res));
+    })
+      .then((res) => console.log(res.json()))
+      .then((data) => console.log(data));
   };
 
   return (
@@ -24,8 +28,10 @@ function App() {
         <input type="text" name="newLink" />
         <button>Shorten</button>
       </form>
-      <input type="text" />
-      <button>copy Link</button>
+      <div className="output">
+        <input type="text" />
+        <button className="copy-button">copy Link</button>
+      </div>
     </div>
   );
 }
