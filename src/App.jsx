7a4490e261c7839e0ebd2abd1bbf0d17e3fbb070
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-04-27 23:52:30 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 const token = process.env.REACT_APP_Access_TOKEN;
 
 const url = "https://api-ssl.bitly.com/v4/shorten";
+
 function App() {
+  const [shortLink, setShortLink] = useState("");
+
   const generateLink = (e) => {
     e.preventDefault();
     const longLink = e.target.newLink.value;
     console.log(longLink);
     console.log(token);
-    fetch("https://api-ssl.bitly.com/v4/shorten", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ long_url: longLink, domain: "bit.ly" }),
-    })
-      .then((res) => console.log(res.json()))
-      .then((data) => console.log(data));
+
+    async function fetchLink() {
+      const response = await fetch("https://api-ssl.bitly.com/v4/shorten", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ long_url: longLink, domain: "bit.ly" }),
+      });
+      const links = await response.json();
+      setShortLink(links.link);
+    }
+    fetchLink();
   };
 
   return (
@@ -29,7 +36,7 @@ function App() {
         <button>Shorten</button>
       </form>
       <div className="output">
-        <input type="text" />
+        <input type="text" value={shortLink ? shortLink : ""} />
         <button className="copy-button">copy Link</button>
       </div>
     </div>
